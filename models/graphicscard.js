271654359341752372b48458c2f79db126543a1b
Hayden Caffey <hayden@Haydens-MBP-2.lan>
2023-05-01 19:24:08 -0400

    initial models: index, graphics, memory

---
+++ models/graphicscard.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class graphicsCard extends Model {}
+
+graphicsCard.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+        type: DataTypes.VARCHAR(250),
+        allowNull: false,
+      },
+    MSRP: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    ourPrice: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+      },
+    quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    clockSpeed: {
+        type: DataTypes.STRING
+    },
+  },
+  {sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'graphicsCard',
+  }
+);
+
+module.exports = graphicsCard;
---
+++ models/index.js
@@ -0,0 +1,11 @@
+const graphicsCard = require('./graphicscard');
+
+const memory = require('./memory');
+
+const cpu = require('./cpu');
+
+module.exports = { 
+    graphicsCard, 
+    memory,
+    cpu 
+};
