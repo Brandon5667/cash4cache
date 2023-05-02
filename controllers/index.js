4761565550f4f1aa71e3b004ca62f998a3014b4d
Brandon Cullifer <brandon.cullifer@gmail.com>
2023-05-02 19:19:18 -0400

    added homepage view

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 
 const homeRoutes = require('./homeRoutes.js');
 const uploadRoutes = require('./uploadRoutes.js');
-const loginRoutes = require('./loginRoutes.js');
-const userRoutes = require('./userRoutes.js');
+const loginRoutes = require('./loginroutes.js');
+const userRoutes = require('./userroutes.js');
 
 router.use('/', homeRoutes);
 router.use('/upload', uploadRoutes);
