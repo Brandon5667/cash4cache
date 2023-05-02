370117d24308d860e325aebb07e40df585e2a51f
unknown <sculptoid11@gmail.com>
2023-05-02 19:12:05 -0400

    fixing loginroute and added login handlebars

--- controllers/loginroutes.js
+++ controllers/loginroutes.js
@@ -3,12 +3,16 @@ const router = require('express').Router();
 const bcrypt = require('bcrypyt');
 
 //changed casing for push 
-//post username/password data to the database when the user creates an account
+//post name/email/password data to the database when the user creates an account
 router.post('/login', async (req, res) => {
   try {
-    const newUser = req.body;
-    newUser.password = await bcrypt.hash(req.body.password, 10);
-    const userData = await db.User.create(newUser);
+    const{name, email, password} = req.body;
+    const passwordCode = await bcrypt.hash(password, 10);
+    const userData = await db.User.create({
+      name,
+      email,
+      password: passwordCode
+  });
 
     req.session.logged_in = true; //registers them as logged in after creating an account
     req.session.user_id = newUser.id;
@@ -23,19 +27,19 @@ router.post('/login', async (req, res) => {
 //post request route to log someone in when they log in
 router.post('/login', async (req, res) => {
   try{
-    const {username, password} = req.body;
+    const {email, password} = req.body;
 
-    const user = await db.User.findOne({where: {username}});
+    const user = await db.User.findOne({where: {email}});
     const validPassword = await bcrypt.compare(
       req.body.password,
       userData.password
       );
 
     if (!user){
-      return res.status(401).render('login', {error: "Incorrect Username or Password."})
+      return res.status(401).render('login', {error: "Incorrect Email or Password."})
     }
     if (!validPassword){
-      return res.status(401).render('login', {error: "Incorrect Username or Password."})
+      return res.status(401).render('login', {error: "Incorrect Email or Password."})
     }
     req.session.logged_in = true; 
     req.session.user_id = user.id;
