df66eaf105f95c1e8f5109cf7f64d51a915f11ac
unknown <sculptoid11@gmail.com>
2023-05-04 19:32:03 -0400

    Basically finished client side login logic, but still need to add a little validation

---
+++ public/js/login.js
@@ -0,0 +1,46 @@
+//Login 
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/controllers/loginroutes', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        if (response.ok) {
+            document.location.replace('/');
+          } else {
+            alert('Failed to log in');
+          }
+    };
+};
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+
+
+//Store created account to db 
+const createFormHandler = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#name-create').value.trim();
+    const email = document.querySelector('#email-create').value.trim();
+    const password = document.querySelector('#password-create').value.trim();
+
+    if (name && email && password) {
+        const response = await fetch('/controllers/loginroutes', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+    };
+
+};
+document
+    .querySelector('.create-form')
+    .addEventListener('submit', createFormHandler);
+
