7fc024f41b30400cb82a8aa6a31679ff614fefb9
Hayden Caffey <hayden@Haydens-MBP-2.lan>
2023-05-08 20:01:25 -0400

    Adding js for CPU

---
+++ public/js/cpu.js
@@ -0,0 +1,36 @@
+const createFormHandler = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#cpu_name').value.trim();
+    console.log(name);
+    const description = document.querySelector('#cpu_description').value.trim();
+    console.log(description);
+    const brand = document.querySelector('#cpu_brand').value.trim();
+    console.log(brand);
+    const MSRP = document.querySelector('#cpu_msrp').value.trim();
+    console.log(MSRP);
+    const price = document.querySelector('#cpu_price').value.trim();
+    console.log(price);
+    const cores = document.querySelector('#cpu_cores').value.trim();
+    console.log(cores);
+    const threads = document.querySelector('#cpu_threads').value.trim();
+    console.log(threads);
+
+    if (name && description && MSRP && price) {
+        const response = await fetch('/upload/cpu', {
+            method: 'POST',
+            body: JSON.stringify({ name, description, MSRP, price, storage }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        console.log("Made Post")
+        if (response.ok) {
+            document.location.replace('/profile');
+        }
+        else {
+            alert("Must enter a valid MSRP and price.")
+        }
+    };
+};
+document
+    .querySelector('#cpu_submit')
+    .addEventListener('click', createFormHandler);
