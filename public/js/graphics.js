07ea3baab3c63a9858101ab991633367997f4871
Hayden Caffey <hayden@Haydens-MBP-2.lan>
2023-05-08 21:15:37 -0400

    adding gpu upload

---
+++ public/js/graphics.js
@@ -0,0 +1,32 @@
+const createFormHandler = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#graphics_name').value.trim();
+    console.log(name);
+    const description = document.querySelector('#graphics_description').value.trim();
+    console.log(description);
+    const MSRP = document.querySelector('#graphics_msrp').value.trim();
+    console.log(MSRP);
+    const price = document.querySelector('#graphics_price').value.trim();
+    console.log(price);
+    const cores = document.querySelector('#graphics_clockSpeed').value.trim();
+    console.log(clockSpeed);
+
+    if (name && description && MSRP && ourPrice && clockSpeed) {
+        const response = await fetch('/upload/gpu', {
+            method: 'POST',
+            body: JSON.stringify({ name, description, MSRP, price, clockSpeed }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        console.log("Made Post")
+        if (response.ok) {
+            document.location.replace('/profile');
+        }
+        else {
+            alert("Must enter a valid MSRP and price.")
+        }
+    };
+};
+document
+    .querySelector('#graphics_submit')
+    .addEventListener('click', createFormHandler);
